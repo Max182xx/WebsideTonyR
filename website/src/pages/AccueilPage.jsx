39764fcdf2881f66c897b34aaf5395a733fc47f3
TONY R <tonyr2004@iMac-de-TONY.local>
2024-09-02 09:53:03 +0200

    modif max

--- website/src/pages/AccueilPage.jsx
+++ website/src/pages/AccueilPage.jsx
@@ -29,42 +29,133 @@ const AccueilPage = () => {
     transform: isClicked ? "translateY(-250px)" : "translateX(0)",
     transition: "transform 0.5s ease-in-out",
   };
-   const item3Style = {
-     transform: isClicked ? "translateX(250px)" : "translateX(0)",
-     transition: "transform 0.5s ease-in-out",
-   };
+  const item3Style = {
+    transform: isClicked ? "translateX(250px)" : "translateX(0)",
+    transition: "transform 0.5s ease-in-out",
+  };
+  const item4Style = {
+    transform: isClicked ? "translateY(250px)" : "translateX(0)",
+    transition: "transform 0.5s ease-in-out",
+  };
   return (
     <>
-      <main className="main">
-        {/* -------- menu ------------*/}
-        <div className="menu" onClick={pop}>
-          <i className="logo"></i>
-        </div>
-        {/* -------- menu items with icons ------------*/}
-        <div className="menu-items">
-          <div className="item1" id="item1" style={item1Style}>
-            <center>
-              <a href="#">
-                <FontAwesomeIcon className="fa" icon={faHouse}  />
-              </a>
-            </center>
+      {!isResponsive ? (
+        <main className="main-accueil">
+          {/* -------- menu ------------*/}
+          <div className="menu" onClick={pop}>
+            <i className="logo"></i>
           </div>
-          <div className="item2" id="item2" style={item2Style}>
-            <center>
-              <a href={PortfolioPage}>
-                <FontAwesomeIcon className="fa" icon={faPalette} />
+          {/* -------- menu items with icons ------------*/}
+          <div className="menu-items">
+            <div className="item1 hover-target" id="item1" style={item1Style}>
+              <a href="apropos">
+                <center>
+                  <FontAwesomeIcon className="fa" icon={faInfo} />
+                  <div className="text-item">
+                    <p> A propos </p>
+                  </div>
+                </center>
               </a>
-            </center>
-          </div>
-          <div className="item3" id="item3" style={item3Style}>
-            <center>
-              <a href={ContactPage}>
-                <FontAwesomeIcon className="fa" icon={faPaintbrush} />
+            </div>
+
+            <div className="item2 hover-target" id="item2" style={item2Style}>
+              <a href="portfolio">
+                <center>
+                  <FontAwesomeIcon className="fa" icon={faPalette} />
+                  <div className="text-item">
+                    <p> Portfolio </p>
+                  </div>
+                </center>
               </a>
-            </center>
+            </div>
+
+            <div className="item3 hover-target" id="item3" style={item3Style}>
+              <a href="asavoir">
+                <center>
+                  <FontAwesomeIcon className="fa" icon={faPaintbrush} />
+                  <div className="text-item">
+                    <p> A savoir </p>
+                  </div>
+                </center>
+              </a>
+            </div>
+
+            <div className="item4 hover-target" id="item4" style={item4Style}>
+              <a href="conctact">
+                <center>
+                  <FontAwesomeIcon className="fa" icon={faEnvelope} />
+                  <div className="text-item">
+                    <p> Contact </p>
+                  </div>
+                </center>
+              </a>
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      ) : (
+        <main className="main-responsive">
+          {/* Éléments spécifiques pour le mode responsive */}
+          <div className="menu-image">
+            <h1>Bienvenue sur le site de TONYR</h1>
+
+            {/* -------- menu ------------*/}
+            <Link to="apropos">
+              <div className="superposed-text-container">
+                <SuperposedText>
+                  <img
+                    className="image-adaptive"
+                    src="../media/femmeJaponaise.png"
+                    alt="Femme japonaise"
+                  />
+                  <span className="text-overlay">A Propos</span>
+                </SuperposedText>
+              </div>
+            </Link>
+
+            {/* -------- Portfolio ------------*/}
+            <Link to="portfolio">
+              <div className="superposed-text-container">
+                <SuperposedText>
+                  <img
+                    src="../media/hulk.png"
+                    className="image-adaptive"
+                    alt="hulk"
+                  />
+                  <span className="text-overlay">Portfolio</span>
+                </SuperposedText>
+              </div>
+            </Link>
+
+            {/* -------- A savoir ------------*/}
+            <Link to="asavoir">
+              <div className="superposed-text-container">
+                <SuperposedText>
+                  <img
+                    src="../media/cobra.png"
+                    className="image-adaptive"
+                    alt="Cabro"
+                  />
+                  <span className="text-overlay">A savoir</span>
+                </SuperposedText>
+              </div>
+            </Link>
+
+            {/* --------Contact ------------*/}
+            <Link to="contact">
+              <div className="superposed-text-container">
+                <SuperposedText>
+                  <img
+                    src="../media/picsou03.png"
+                    className="image-adaptive"
+                    alt="Piscsou"
+                  />
+                  <span className="text-overlay">Contact</span>
+                </SuperposedText>
+              </div>
+            </Link>
+          </div>
+        </main>
+      )}
     </>
   );
 };
